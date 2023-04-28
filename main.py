53729a2e79782d38c2e3bcd489e6f531e5098216
vadimdibcov859 <vadimdibcov859@gmail.com>
2023-04-29 04:01:51 +0500

    +

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def start_task(start_number, end_number):
 class GetNumber(Resource):
     def get(self, phone_id):
         if phone_id in phone_numbers:
-            return phone_numbers[phone_id]
+            return '+' + phone_numbers[phone_id]
         else:
             return 'TO DO "EMPTY" NUMBER'
 
