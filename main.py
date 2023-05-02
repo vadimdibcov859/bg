b9c161d334e1aabc9a284c92d95d02a89343edf1
vadimdibcov859 <vadimdibcov859@gmail.com>
2023-05-03 01:10:43 +0500

    -> iphone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import gmail
 import os.path
 import threading
 
@@ -15,32 +16,14 @@ server_threads_count = 128
 
 phone_numbers = dict()
 
-country = [380, 998, 1, 86, 49, 33, 81]
 
-
-def detect_county(number):
-    for code in country:
-        if str(code) == str(number)[:len(str(code))]:
-            return code
-
-    return 0
-
-
-def add_number(number, is_an_apple_num):
-    with open('checked_numbers.txt', 'r') as checked_numbers_file:
-        if str(number) in checked_numbers_file.read().split():
-            return
-
-    country_data_file = f'{detect_county(number)}{["green", "blue"][is_an_apple_num]}.txt'
-
-    if not os.path.exists('data'):
-        os.mkdir('data')
-
-    with open(os.path.join('data', country_data_file), 'a') as file:
-        file.write(str(number) + '\n')
-
-    with open('checked_numbers.txt', 'a') as checked_numbers_file:
-        checked_numbers_file.write(str(number) + '\n')
+def start_preparing_mails():
+    while True:
+        try:
+            gmail.prepare_inbox()
+            time.sleep(60)
+        except Exception as error:
+            print('prepare mail error: ', error)
 
 
 def start_task(start_number, end_number):
@@ -67,14 +50,14 @@ def start_task(start_number, end_number):
 class GetNumber(Resource):
     def get(self, phone_id):
         if phone_id in phone_numbers:
-            return phone_numbers[phone_id]
+            return '+' + phone_numbers[phone_id]
         else:
-            return 'TO DO "EMPTY" NUMBER'
+            return ' '
 
 
 class StartTask(Resource):
     def get(self, start_number, end_number):
-        if threading.active_count() == (server_threads_count + 1):
+        if threading.active_count() == (server_threads_count + 1 + 1):
             if end_number >= start_number:
                 threading.Thread(target=start_task, args=[start_number, end_number]).start()
                 return {'started': True}
@@ -98,23 +81,12 @@ class SetPhonesCount(Resource):
             return data.write(str(phones_count))
 
 
-class AddGreen(Resource):
-    def get(self, number):
-        add_number(number, False)
-
-
-class AddBlue(Resource):
-    def get(self, number):
-        add_number(number, True)
-
-
 api.add_resource(GetNumber, '/get_number/<int:phone_id>')
 api.add_resource(StartTask, '/start_task/<int:start_number>/<int:end_number>/')
 api.add_resource(GetData, '/get_data/<string:country>')
 api.add_resource(SetPhonesCount, '/set_max_id/<int:phones_count>')
-api.add_resource(AddGreen, '/add_green/<int:number>')
-api.add_resource(AddBlue, '/add_blue/<int:number>')
 api.init_app(app)
 
 if __name__ == '__main__':
+    threading.Thread(target=start_preparing_mails).start()
     serve(app, host="0.0.0.0", port=80, threads=server_threads_count)
